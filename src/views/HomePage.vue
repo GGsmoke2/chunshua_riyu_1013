8d37f000488db85b71811b3a56f8e473f8d601ba
GGsmoke2 <19818941549@163.com>
2025-10-13 21:36:06 +0800

    Updated HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import HeroBanner from '../components/home/HeroBanner.vue'
-import FeatureCards from '../components/home/FeatureCards.vue'
 import QualificationSection from '../components/home/QualificationSection.vue'
 import TeacherQualification from '../components/home/TeacherQualification.vue'
 </script>
@@ -8,7 +7,6 @@ import TeacherQualification from '../components/home/TeacherQualification.vue'
 <template>
   <div class="home-page">
     <HeroBanner />
-    <FeatureCards />
     <QualificationSection />
     <TeacherQualification />
   </div>
